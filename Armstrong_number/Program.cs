daee569332f8d42ed325ec8b3ed7acafcdadbd45
Rahul Rai <rairayrahul22@gmail.com>
2025-02-17 10:52:48 +0530

    Add project files.

---
+++ Armstrong_number/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Armstrong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0, temp = num, digits = num.ToString().Length;
+
+            while (temp > 0)
+            {
+                int remainder = temp % 10;
+                sum += (int)Math.Pow(remainder, digits);
+                temp = temp / 10;
+            }
+
+            if (num == sum)
+            {
+                Console.WriteLine(num + " is an Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not Armstrong Number");
+            }
+        }
+    }
+}
---
+++ Coditional_Statements/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Coditional_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 20;
+            int m = 15;
+
+            if (n < m)
+            {
+                Console.WriteLine("n is less than m");
+            }
+            else if (n == m)
+            {
+                Console.WriteLine();
+            }
+            else {
+                Console.WriteLine("n is greater than m");
+            }
+
+        }
+    }
+}
